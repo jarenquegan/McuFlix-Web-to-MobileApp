42f8720a1c027d16c4ac46475d3e2b2d64fcaa76
Jaren Quegan <queganjaren@gmail.com>
2025-02-11 12:04:04 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { Network } from '@awesome-cordova-plugins/network/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    StatusBar,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    InAppBrowser,
+    Network,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  InAppBrowser,
+  InAppBrowserOptions,
+} from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { Platform } from '@ionic/angular';
+import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  subscription: any;
 
-  constructor() {}
+  constructor(
+    private inAppBrowser: InAppBrowser,
+    private platform: Platform,
+    private statusBar: StatusBar
+  ) {}
 
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      this.changeStatusBarColor();
+      this.launchsite();
+    });
+  }
+
+  changeStatusBarColor() {
+    this.statusBar.backgroundColorByHexString('#000000');
+  }
+
+  launchsite() {
+    const options: InAppBrowserOptions = {
+      hardwareback: 'yes',
+      zoom: 'no',
+      allowInlineMediaPlayback: 'no',
+      location: 'no',
+      toolbar: 'no',
+      fullscreen: 'no',
+      hideurlbar: 'yes',
+    };
+
+    const url = 'http://192.168.125.86/mcuflix/'; // palitan mo lang po ito, Sir
+
+    const browser = this.inAppBrowser.create(url, '_self', options);
+
+    browser.on('loadstop').subscribe((event) => {
+      browser.executeScript({
+        code: `
+        document.addEventListener('click', function(event) {
+          const target = event.target;
+          if (target.tagName.toLowerCase() === 'a') {
+            const url = target.href;
+            const fileExtensions = ['.pdf', '.docx', '.xlsx', '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.zip', '.rar', '.tar', '.gz', '.mp4'];
+            const isDownloadable = fileExtensions.some(ext => url.includes(ext));
+            if (isDownloadable) {
+              event.preventDefault();
+              window.open(url, '_system');
+            }
+          }
+        }, false);
+      `,
+      });
+    });
+
+    browser.on('loadstop').subscribe(() => {
+      browser.executeScript({
+        code: `
+          const style = document.createElement('style');
+          style.innerHTML = \`
+            *:focus {
+              outline: none;
+            }
+            *:active {
+              outline: none;
+            }
+          \`;
+          document.head.appendChild(style);
+        `,
+      });
+    });
+  }
+
+  ionViewWillLeave() {
+    this.subscription.unsubscribe();
+  }
 }
